d5f464a99fa8e45d69ff7def5cdc468ec1348ea1
reza akhmad gandara <rezaakhmadg@gmail.com>
2020-04-06 19:49:07 +0700

    sudah mulai lelah saudara

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const express   = require('express')
+const bodyParser = require('body-parser')
+const axios     = require('axios')
 
-const await = require('await');
+const server = express()
+server.use(bodyParser.urlencoded({
+    extended:true
+}))
+server.use(bodyParser.json());
+
+const await     = require('await');
 const translate = require('translate');
 
 const Sentiment = require('sentiment');
@@ -10,12 +19,42 @@ const sentiment = new Sentiment();
 
 translate.engine = 'google';
 translate.key = 'AIzaSyAWZB-eFeo5mQt-dRuCvVeOeMgalpIgRdQ';
- 
-let text = "saya benci banget sama kamu";
-translate(text, { from: 'id', to: 'en' }).then(textTranslate => {
-    text=textTranslate  // Pingin di ganti isi text nya jadi english
-    let result = sentiment.analyze(text);
-    console.log(text)
-    console.log(result)
-  });
+
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTQ4MTMsInRpbWVzdGFtcCI6IjIwMjAtMDMtMTNUMTc6NTQ6MTcuNDY0KzA3OjAwIn0.mpvSmT8_STMk7pz5PIxff1NMz4Zz1PGW-xr0_vqGNjs'
+const url = 'https://api.chataja.co.id/api/v1/chat/conversations/'
+
+server.post('/',(req,res)=>{
+    let room_id = res.req.body.chat_room.qiscus_room_id
+    let msg = res.req.body.message.text
+    let nama = res.req.body.from.fullname
+    
+    if(msg !== null ){
+        
+        axios.post(url+'post_comment',{
+            access_token: token,
+            topic_id:room_id,
+            comment: "Score dari kata kamu adalah "+ myFunction()
+        }).then((hsl)=>{
+            console.log(hsl.data.data.username)
+        }).catch((err)=>{
+            //error
+        })
+    }
+
+    async function myFunction(){
+        msg = await translate(msg,{from: "id", to: "en"});
+        let dibaca = sentiment.analyze(msg)
+        // return dibaca.score;
+        console.log(dibaca.score);
+    }
+    
+})
+
+server.listen((process.env.PORT || 3000), () => {
+    console.log("Server is up and running...");
+});
+
+
+
+
 
